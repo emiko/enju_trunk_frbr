f8edf61180821f9c77a2c2486e0bc47462c660ad
asoda <asoda.masataka@miraitsystems.jp>
2014-03-12 17:41:00 +0900

    add validate

--- app/models/exemplify.rb
+++ app/models/exemplify.rb
@@ -4,7 +4,16 @@ class Exemplify < ActiveRecord::Base
 
   validates_associated :manifestation, :item
   validates_presence_of :manifestation_id, :item_id
-  validates_uniqueness_of :item_id
+  # validates_uniqueness_of :item_id
+
+  # TODO
+  logger.error "############### Frbr_exemplify! ###############"
+  validates_uniqueness_of :manifestation_id,
+    :if => proc { logger.error SystemConfiguration.get("manifestation.has_one_item");logger.error "############################";SystemConfiguration.get("manifestation.has_one_item") }
+  validates_uniqueness_of :item_id,
+    :if => proc { logger.error SystemConfiguration.get("manifestation.has_one_item");logger.error "############################";SystemConfiguration.get("manifestation.has_one_item") }
+  # TODO
+
   after_save :reindex
   after_destroy :reindex
   after_create :create_lending_policy, :unless => proc{SystemConfiguration.isWebOPAC}
